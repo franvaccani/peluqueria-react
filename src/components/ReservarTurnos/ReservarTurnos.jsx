6cf05385844203d8bf1004fc02d8af6b61f5bbf6
franvaccani <franc@Mac-mini.local>
2025-01-16 21:16:08 -0300

    .

--- src/components/ReservarTurnos/ReservarTurnos.jsx
+++ src/components/ReservarTurnos/ReservarTurnos.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { reservarTurno } from "../../firebase/firebaseFunctions"; // Asegúrate de tener esta función configurada correctamente
-import { crearPago } from "../../api/mercadoPago"; // La función que genera el enlace de pago
 import { db } from "../../firebase/config"; // Configuración de Firebase
 import { collection, getDocs } from "firebase/firestore"; // Para obtener datos de Firebase
 
@@ -12,7 +11,6 @@ const ReservaTurnos = () => {
   const [fecha, setFecha] = useState("");
   const [hora, setHora] = useState("");
   const [servicio, setServicio] = useState("");
-  const [precio, setPrecio] = useState(""); // Precio del servicio
   const [message, setMessage] = useState("");
 
   // Cargar peluqueros desde Firebase al cargar el componente
@@ -37,32 +35,11 @@ const ReservaTurnos = () => {
     e.preventDefault();
 
     try {
-      // Guardar el turno en Firebase (con todos los datos)
-      const result = await reservarTurno(
-        cliente,
-        email,
-        peluquero,
-        fecha,
-        hora,
-        servicio,
-        precio
-      );
+      // Guardar el turno en Firebase
+      const result = await reservarTurno(cliente, email, peluquero, fecha, hora, servicio);
 
       if (result.success) {
-        const turno = {
-          peluquero,
-          precio,
-        };
-
-        // Crear el pago con Mercado Pago, solo con el nombre del peluquero y el precio
-        const paymentLink = await crearPago(turno);
-
-        if (paymentLink) {
-          setMessage("Turno reservado con éxito. Redirigiendo al pago...");
-          window.location.href = paymentLink; // Redirigir al checkout de Mercado Pago
-        } else {
-          setMessage("Hubo un problema al generar el enlace de pago.");
-        }
+        setMessage("Turno reservado con éxito.");
       } else {
         setMessage("Hubo un problema al reservar el turno.");
       }
@@ -163,18 +140,6 @@ const ReservaTurnos = () => {
               type="text"
               value={servicio}
               onChange={(e) => setServicio(e.target.value)}
-              required
-              className="mt-2 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm"
-            />
-          </div>
-
-          {/* Precio */}
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Precio</label>
-            <input
-              type="number"
-              value={precio}
-              onChange={(e) => setPrecio(e.target.value)}
               required
               className="mt-2 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm"
             />
